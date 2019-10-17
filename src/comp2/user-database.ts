6c8675698dcb27fcab2ed5098b74b28173eb623a
Cristian B <cbetivu@gmail.com>
2019-10-17 23:53:54 +0200

    Add comment

--- src/comp2/user-database.ts
+++ src/comp2/user-database.ts
@@ -1,5 +1,6 @@
 import { Person } from '../types'
 
 export const data = new Map<string, Person>([
+    // not very good idea to replicate the CPR, but this allows us to fake a search by ID in the database
     ['123', { cpr: '123', firstName: 'Bob', lastName: 'Alice' }]
 ])
